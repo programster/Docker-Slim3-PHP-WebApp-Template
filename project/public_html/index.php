2aa3af20a5e83f9d3c6a39b4eb284a3232726181
Stuart Page <stuart.page@irap.org>
2015-04-10 20:47:39 +0000

    bugfix in index.php

--- project/public_html/index.php
+++ project/public_html/index.php
@@ -4,7 +4,7 @@ require_once(__DIR__ . '/../bootstrap.php');
 
 $app = new \Slim\Slim();
 
-$app->get('/', function ($name) {
+$app->get('/', function () {
     echo "Welcome to the programster's default template.";
 });
 
