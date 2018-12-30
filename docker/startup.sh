864d09cd26743b3e4e4717ce998cc5fe50771600
Programster <sdpagent@gmail.com>
2018-12-30 14:23:08 +0000

    fix issue with apache not running when container deployed.

--- docker/startup.sh
+++ docker/startup.sh
@@ -2,7 +2,7 @@
 # This script is run by the docker container when it is "run"
 
 # Run the apache process in the background
-/usr/sbin/apache2 -D APACHE_PROCESS &
+service apache2 start
 
 
 # Put any startup tasks here!
