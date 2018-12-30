b3f92b29025255f674e8b60b2aa91b046d3a1438
Programster <sdpagent@gmail.com>
2018-12-30 14:14:39 +0000

    Updates bootstrap to quit out if the ENVIRONMENT variable has not been set. Will display errors or not based on the ENVIRONMENT variable.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -12,6 +12,27 @@
 require_once(__DIR__ . '/vendor/autoload.php'); # this autoloads all vendor packages
 require_once(__DIR__ . '/settings.php');
 
+$environment = getenv("ENVIRONMENT");
+
+if ($environment === FALSE)
+{
+    die("Incorrect configuration: ENVIRONMENT not set. Please check your .env file.");
+}
+
+define("ENVIRONMENT", $environment);
+
+if (in_array("ENVIRONMENT", ["dev", "development", "staging", "release"]))
+{
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
+else
+{
+    ini_set('display_errors', 0);
+    ini_set('display_startup_errors', 0);
+}
+
 
 // Define which folders contain all of our php classes.
 $classDirs = array(
